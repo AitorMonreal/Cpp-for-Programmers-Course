18bc0553f3e815aabebb68c35f33eb5102452816
AitorMonreal <55390029+AitorMonreal@users.noreply.github.com>
2020-12-30 22:50:52 +0000

    Update main.cpp

--- TicTacToe_Quiz/main.cpp
+++ TicTacToe_Quiz/main.cpp
@@ -1,8 +1,33 @@
 
-#include "mainClasses.cpp"
 #include "mainFunctions.cpp"
 
 int main(){
-    game Gameboard
-    checkIfWinner();
+    Gameboard game;
+    bool finish = 0;
+    int row;
+    int column;
+    int count = 0;
+    int max_turns = 8;
+    
+    finish = checkIfWinner(game);
+    while(count<max_turns){
+        if(finish == 0){
+            count += 1;
+            std::cout << "\n\n\nX player:\n- Enter row:\n";
+            std::cin >> row;
+            std::cout << "- Enter column:\n";
+            std::cin >> column;
+            game.setGameSpace(row, column, 'x');
+            std::cout << "O player:\n- Enter row:\n";
+            std::cin >> row;
+            std::cout << "- Enter column:\n";
+            std::cin >> column;
+            game.setGameSpace(row, column, 'o');
+            game.printInfo();
+        }
+        else{
+            std::cout<<"Game Won";
+            break;
+        }
+    }
 }
