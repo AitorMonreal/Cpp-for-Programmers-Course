580988183eed431a0d21feee68b4132f7ceedcc5
AitorMonreal <55390029+AitorMonreal@users.noreply.github.com>
2021-01-25 17:41:15 +0000

    Update main.cpp

--- Luhn-Algorithm/main.cpp
+++ Luhn-Algorithm/main.cpp
@@ -1,33 +1,15 @@
 #include <iostream>
-constexpr int numberLength = 10;
-
-int* AccountInput()
-{
-	int accountNumber[numberLength];
-	for (int i = 0; i < numberLength; i++)
-	{
-		std::cout << "Enter the next number for the account\n";
-		std::cin >> accountNumber[i];
-	}
-	return accountNumber;
-}
-
-void CheckAccount(int* accountNumber)
-{
-	DoubleValue(int* accountNumber);
-	SumElements(int* accountNumber);
-	FindModulo(int* accountNumber);
-}
-
-void DoubleValue(int* accountNumber)
-{
-	
-}
+#include "mainFunctions.h"
 
 int main()
 {
 	int* accountNumber = AccountInput();
-	CheckAccount(accountNumber);
+	bool result = CheckAccount(accountNumber);
+	if (result == true)
+		std::cout << "Account passed the check.\n";
+	else
+		std::cout << "Account failed to pass the check.\n";
+
 	std::cin.get();
 	return 0;
 }
