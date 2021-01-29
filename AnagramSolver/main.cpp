afd5dc9886457f39472cb493f3f8d9ab391d240f
AitorMonreal <55390029+AitorMonreal@users.noreply.github.com>
2021-01-29 00:25:25 +0000

    Add check to see if base word is in the map
    Needs cleaning and moving everything in main into functions

--- AnagramSolver/main.cpp
+++ AnagramSolver/main.cpp
@@ -4,7 +4,7 @@
 #include <map>
 #include <vector>
 
-constexpr int s_Length = 6;
+constexpr int s_Length = 10;
 
 void ReorderString(std::string& word);
 void InsertionSort(std::string& word, int* ptrLength);
@@ -18,15 +18,15 @@ void ReorderList(std::array<std::string, s_Length>& list)
 
 void ReorderString(std::string& word)
 {
-	int wordLength = 0;
-	int* ptrWordLength = &wordLength;
-	//std::sort(word.begin(), word.end());
-	for (std::string::iterator it = word.begin(); it != word.end(); ++it)
-	{
-		//std::cout << *it << "\n";
-		wordLength += 1;
-	}
-	InsertionSort(word, ptrWordLength);
+int wordLength = 0;
+int* ptrWordLength = &wordLength;
+//std::sort(word.begin(), word.end());
+for (std::string::iterator it = word.begin(); it != word.end(); ++it)
+{
+	//std::cout << *it << "\n";
+	wordLength += 1;
+}
+InsertionSort(word, ptrWordLength);
 }
 
 void InsertionSort(std::string& word, int* ptrLength) {
@@ -53,53 +53,83 @@ std::ostream& operator<<(std::ostream& stream, const std::vector<std::string>& w
 	std::vector<std::string>::const_iterator it;
 	for (it = wordVector.begin(); it != wordVector.end(); it++)
 		stream << *it << ", ";
-	stream << "endvector\n";
+	//stream << "\n";
 	return stream;
 }
 
 int main()
 {
 	//static const int s_Length = 6;
-	std::array<std::string, s_Length> wordList{ "blue", "red", "orange", "yellow", "silent", "listen"};
+	std::array<std::string, s_Length> wordList{ "blue", "red", "orange", "yellow", "silent", "listen", "arc", "car", "below", "night" };
+	std::cout << "List of words:\n";
+	for (int i = 0; i < s_Length; i++)
+		std::cout << wordList[i] << "\n";
+
 	std::string baseWord;
-	std::cout << "Enter the word for which you want to search anagrams for in the list:\n";
+	std::cout << "\nEnter the word for which you want to search anagrams for in the list:\n";
 	std::cin >> baseWord;
 	ReorderString(baseWord);
 
 	ReorderList(wordList);
-	for (int i = 0; i < s_Length; i++) {
+	std::cout << "\nReordered list of words:\n";
+	for (int i = 0; i < s_Length; i++)
 		std::cout << wordList[i] << "\n";
-	}
-	
+
 	//std::map<char, std::string> sortedWordsMap{ std::make_pair(wordList[0][0], wordList[0]) };
 	//std::vector<std::string> wordVector({ wordList[0] });
 	//std::map<char, std::vector<std::string>> sortedWordsMap{ std::make_pair(wordList[0][0], wordVector) };
-	
-	std::map<char, std::vector<std::string>> sortedWordsMap;
-	std::vector<std::string> wordVector({ wordList[0] });
 
-	int elementIndex;
+	//Create the Map of words
+	std::map<char, std::vector<std::string>> sortedWordsMap;
+	bool present;
 	for (const std::string& word : wordList)  // const auto& is used because you're not modifying word. This also avoid making deep-copies of word - this is generally ok for ints or doubles because making copies is cheap, but this is not the case for strings, for instance.
 	{
-		elementIndex = 0;
-		bool present = false;
+		present = false;
 		for (const auto& element : sortedWordsMap)
 		{
 			if (element.first == word[0])
 			{
 				present = true;
-				sortedWordsMap[elementIndex].push_back(word);
-				wordVector = element.second;
+				sortedWordsMap[element.first].push_back(word);  //map[key] == element in map for that key
 			}
-			elementIndex += 1;
 		}
 		if (present == false)
 			sortedWordsMap.insert(std::pair<char, std::vector<std::string>>(word[0], { word }));
 	}
-	std::cout << "MAP:\n\n";
+	std::cout << "\nWord Map:\n";
 	for (const auto& element : sortedWordsMap)
-		std::cout << std::vector<std::string>(element.second);
-		//std::cout << element.first << "      " << std::vector<std::string>(element.second) << "\n";
+		//std::cout << std::vector<std::string>(element.second);
+		std::cout << element.first << "      " << std::vector<std::string>(element.second) << "\n";
+
+	//Search for the base word in the Map, first finding the letter which it belongs to, and then in those elements
+	present = false;
+	for (const auto& element : sortedWordsMap)
+	{
+		if (element.first == baseWord[0]) {
+			present = true;
+			break;
+		}
+	}
+
+	if (present == true)
+	{
+		present = false;
+		for (const auto& word : sortedWordsMap[baseWord[0]])
+		{
+			if (baseWord == word)
+			{
+				present = true;
+				break;
+}
+		}
+
+	}
+
+	if (present == true)
+		std::cout << "\n\nThe word has an anagram in the list.\n\n";
+
+	else
+		std::cout << "\n\nNo anagram found in the list.\n\n";
 
 	std::cin.get();
 }
